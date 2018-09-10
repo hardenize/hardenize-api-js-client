ac502dabb7a17391dedd6dacff3862467a09ff51
Mike Cardwell <mike.cardwell@hardenize.com>
2018-09-10 11:21:07 +0100

    Create cert end-point now uses POST

--- src/certs/addCert.js
+++ src/certs/addCert.js
@@ -5,7 +5,7 @@ module.exports = function addCert(pem){
     if (pem.length === 0) throw 'Unable to parse certificate';
 
     return this.apiCall({ path: 'certs/', validStatus: [ 201, 202 ] }, {
-        method:  'put',
+        method:  'post',
         headers: { 'Content-Type': 'application/x-pem-file' },
         body:    pem,
     });
