83598283946caa92f44e59f98227b9f383590a6d
Mike Cardwell <mike.cardwell@hardenize.com>
2018-09-10 12:07:07 +0100

    Update getHost for new API end-point

--- src/hosts/getHost.js
+++ src/hosts/getHost.js
@@ -1,4 +1,4 @@
 module.exports = function getHost(hostname){
     if (typeof hostname !== 'string') throw 'Invalid hostname';
-    return this.apiCall({ path: 'host/' + encodeURIComponent(hostname), validStatus: 200 });
+    return this.apiCall({ path: 'hosts/' + encodeURIComponent(hostname), validStatus: 200 });
 };
