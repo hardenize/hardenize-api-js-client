f152d351bbabb769dda4bab9c30a4b1aab9c98e6
Mike Cardwell <mike.cardwell@hardenize.com>
2020-01-21 16:06:26 +0000

    Allow no options when calling getHdbCertBySha256

--- src/hdb/certs/getHdbCertBySha256.js
+++ src/hdb/certs/getHdbCertBySha256.js
@@ -1,6 +1,6 @@
 module.exports = function getHdbCertBySha256(sha256, options){
     var url = 'hdb/certs/certBySha256/' + encodeURIComponent(sha256.toLowerCase());
-    if (options.unpacked) url += '?includeUnpacked=true';
+    if (options && options.unpacked) url += '?includeUnpacked=true';
 
     return this.apiCall({ url: url, validStatus: 200 });
 };
