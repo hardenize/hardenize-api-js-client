55dc49c2778295ec52b7f58ba55103702bc6de96
Mike Cardwell <mike.cardwell@hardenize.com>
2020-01-21 15:11:53 +0000

    getHdbCertBySha256 end-point takes includeUnpacked, and doesn't take includePem

--- src/hdb/certs/getHdbCertBySha256.js
+++ src/hdb/certs/getHdbCertBySha256.js
@@ -1,6 +1,6 @@
 module.exports = function getHdbCertBySha256(sha256, options){
     var url = 'hdb/certs/certBySha256/' + encodeURIComponent(sha256.toLowerCase());
-    if (options.pem) url += '?includePem=true';
+    if (options.unpacked) url += '?includeUnpacked=true';
 
     return this.apiCall({ url: url, validStatus: 200 });
 };
